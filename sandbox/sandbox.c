525ea7bd25cdeb5f60fff9bab7c238c505903fea
Maria Fornovi Gomez <mfornovi@c2r11s6.42madrid.com>
2026-03-16 13:02:37 +0100

    studying, studying

---
+++ sandbox/sandbox.c
@@ -0,0 +1,46 @@
+#include <unistd.h>     
+#include <sys/wait.h>   
+#include <signal.h>     
+#include <stdbool.h>    
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <errno.h>      
+#include <string.h>   
+
+static void do_nothing(int sig)
+{
+	(void)sig;
+}
+
+int	sandbox(void (*f)(void), unsigned int timeout, bool verbose)
+{
+	if (!f)
+		return(-1);
+	int		status;
+	pid_t	pid = fork();
+
+	if(pid < 0)
+		return(-1);
+	if (pid == 0)
+	{
+		alarm(timeout);
+		f();
+		_exit(0);
+	}
+	struct sigaction sa = {0};
+	sa.sa_handler = do_nothing;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGALRM, &sa, NULL) < 0)
+		return (-1);
+	alarm(timeout);
+	pid_t r = waitpid(pid, &status, 0);
+	if (r < 0)
+	{
+		if (errno == EINTR)
+		{
+			kill(pid, SIGKILL);
+			
+		}
+	}
+}
