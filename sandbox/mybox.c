265e8e5cb4d9e6d12dc33a204e961ce009811683
Maria Fornovi Gomez <mfornovi@c2r11s5.42madrid.com>
2026-03-17 18:28:25 +0100

    studying, studying, sandbox currently still a mess

--- sandbox/mybox.c
+++ sandbox/mybox.c
@@ -1,9 +1,8 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <signal.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <errno.h>
 #include <stdbool.h>
 
@@ -15,24 +14,24 @@ void do_nothing(int sig)
 int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
 	if (!f)
-		return(-1);
+		return -1;
 	int status;
-	pid_t pid;
-	pid = fork();
+	pid_t pid = fork();
+
 	if (pid < 0)
-		return(-1);
+		return -1;
 	if (pid == 0)
 	{
 		alarm(timeout);
 		f();
 		_exit(0);
 	}
-	struct sigaction sa;
+	struct sigaction sa = {0};
 	sa.sa_handler = do_nothing;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	if (sigaction(SIGALRM, &sa, NULL) < 0)
-		return (-1);
+		return -1;
 	alarm(timeout);
 	pid_t r = waitpid(pid, &status, 0);
 	if (r < 0)
@@ -43,28 +42,28 @@ int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 			waitpid(pid, NULL, 0);
 			if (verbose)
 				printf("Bad function: timed out after %u seconds\n", timeout);
-			return (0);
+			return 0;
 		}
-		return (-1);
+		return -1;
 	}
 	alarm(0);
 	if (WIFEXITED(status))
 	{
 		int code = WEXITSTATUS(status);
-		if (code == 0)
+		if (code ==  0)
 		{
 			if (verbose)
-				printf("Nice function!\n");
-			return(1);
+				printf("Nice function!", code);
+			return 1;
 		}
 		if (verbose)
 			printf("Bad function: exited with code %d\n", code);
-		return (0);
+		return 0;
 	}
-	if (WIFSIGNALED(status))
+	if (WIFSIGNALES(status))
 	{
 		int sig = WTERMSIG(status);
-		if (sig == SIGALRM)
+		if (sig == (SIGALRM))
 		{
 			if (verbose)
 				printf("Bad function: timed out after %u seconds\n", timeout);
@@ -74,7 +73,7 @@ int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 			if (verbose)
 				printf("Bad function: %s\n", strsignal(sig));
 		}
-		return(0);
+		return 0;
 	}
-	return (-1);
+	returned -1;
 }
