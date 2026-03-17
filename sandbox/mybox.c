021dc5bef04a35976a298f7129408e41d9a7b49d
Maria Fornovi Gomez <mfornovi@c2r11s6.42madrid.com>
2026-03-17 11:47:23 +0100

    studying, studying, trying out sandbox for the first time

--- sandbox/mybox.c
+++ sandbox/mybox.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <errno.h>
 #include <stdbool.h>
@@ -16,25 +16,65 @@ int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
 	if (!f)
 		return(-1);
+	int status;
 	pid_t pid;
 	pid = fork();
 	if (pid < 0)
-	{
-		close(fd[1]);
-		close(fd[0]);
 		return(-1);
-	}
 	if (pid == 0)
 	{
-		alarm(timeout)
+		alarm(timeout);
 		f();
 		_exit(0);
 	}
 	struct sigaction sa;
 	sa.sa_handler = do_nothing;
-	sigemptyset(&sa.sa_masks);
+	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	if (sigaction(SIGALRM, &sa, NULL) < 0)
 		return (-1);
 	alarm(timeout);
+	pid_t r = waitpid(pid, &status, 0);
+	if (r < 0)
+	{
+		if (errno == EINTR)
+		{
+			kill(pid, SIGKILL);
+			waitpid(pid, NULL, 0);
+			if (verbose)
+				printf("Bad function: timed out after %u seconds\n", timeout);
+			return (0);
+		}
+		return (-1);
+	}
+	alarm(0);
+	if (WIFEXITED(status))
+	{
+		int code = WEXITSTATUS(status);
+		if (code == 0)
+		{
+			if (verbose)
+				printf("Nice function!\n");
+			return(1);
+		}
+		if (verbose)
+			printf("Bad function: exited with code %d\n", code);
+		return (0);
+	}
+	if (WIFSIGNALED(status))
+	{
+		int sig = WTERMSIG(status);
+		if (sig == SIGALRM)
+		{
+			if (verbose)
+				printf("Bad function: timed out after %u seconds\n", timeout);
+		}
+		else
+		{
+			if (verbose)
+				printf("Bad function: %s\n", strsignal(sig));
+		}
+		return(0);
+	}
+	return (-1);
 }
