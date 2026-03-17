781f5a896e1f62449e412019c7ef7e3271d3619c
Maria Fornovi Gomez <mfornovi@c2r11s6.42madrid.com>
2026-03-17 11:00:52 +0100

    studying, studying, almost got the popen right first try

--- ft_popen.c/my_popen.c
+++ ft_popen.c/my_popen.c
@@ -1,22 +1,22 @@
 #include <unistd.h>
-#include <stdlib.h>
 #include <sys/types.h>
+#include <stdlib.h>
 
 int ft_popen(const char *file, char *const argv[], char type)
 {
-	int		fd[2];
-	pid_t	pid;
+	if (!file || !argv || (type != 'r' && type != 'w'))
+		return -1;
+	int	fd[2];
+	pid_t pid;
 
-	if (!file || !argv || (type != 'r'  && type != 'w'))
-		return (-1);
 	if (pipe(fd) < 0)
-		return (-1);
+		return -1;
 	pid = fork();
 	if (pid < 0)
 	{
 		close(fd[1]);
 		close(fd[0]);
-		return (-1);
+		return -1;
 	}
 	if (pid == 0)
 	{
@@ -24,27 +24,38 @@ int ft_popen(const char *file, char *const argv[], char type)
 		{
 			close(fd[0]);
 			if (dup2(fd[1], STDOUT_FILENO) < 0)
-				return (-1);
+			{
+				close(fd[0]);
+				close(fd[1]);
+				return -1;
+			}
 		}
 		else
 		{
 			close(fd[1]);
 			if (dup2(fd[0], STDIN_FILENO) < 0)
-				return(-1);
+			{
+				close(fd[0]);
+				close(fd[1]);
+				return -1;
+			}
 		}
 		close(fd[0]);
 		close(fd[1]);
 		execvp(file, argv);
 		exit(-1);
 	}
-	if (type == 'r')
-	{
-		close(fd[1]);
-		return (fd[0]);
-	}
 	else
 	{
-		close(fd[0]);
-		return(fd[1]);
+		if (type == 'r')
+		{
+			close(fd[1]);
+			return (fd[0]);
+		}
+		else
+		{
+			close(fd[0]);
+			return (fd[1]);
+		}
 	}
 }
