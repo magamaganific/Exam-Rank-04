7436b06b961c47e9bb73ca9eb89450a7069ac95a
Maria Fornovi Gomez <mfornovi@c2r11s6.42madrid.com>
2026-03-18 11:04:37 +0100

    studying, studying, sandbox close to perfect attempt ^w^

--- sandbox/mybox.c
+++ sandbox/mybox.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include <string.h>
 #include <errno.h>
 #include <stdbool.h>
 
-void do_nothing(int sig)
+static void do_nothing(int sig)
 {
 	(void)sig;
 }
@@ -14,24 +15,25 @@ void do_nothing(int sig)
 int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
 	if (!f)
-		return -1;
+		return (-1);
 	int status;
 	pid_t pid = fork();
 
 	if (pid < 0)
-		return -1;
+		return (-1);
 	if (pid == 0)
 	{
 		alarm(timeout);
 		f();
 		_exit(0);
 	}
+	alarm(timeout);
 	struct sigaction sa = {0};
 	sa.sa_handler = do_nothing;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	if (sigaction(SIGALRM, &sa, NULL) < 0)
-		return -1;
+		return (-1);
 	alarm(timeout);
 	pid_t r = waitpid(pid, &status, 0);
 	if (r < 0)
@@ -42,38 +44,37 @@ int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 			waitpid(pid, NULL, 0);
 			if (verbose)
 				printf("Bad function: timed out after %u seconds\n", timeout);
-			return 0;
+			return (0);
 		}
-		return -1;
+		return (-1);
 	}
-	alarm(0);
+	alarm(0)
 	if (WIFEXITED(status))
 	{
 		int code = WEXITSTATUS(status);
-		if (code ==  0)
+		if (code == 0)
 		{
 			if (verbose)
-				printf("Nice function!", code);
-			return 1;
+				printf("Nice function!\n");
+			return (1);
 		}
 		if (verbose)
 			printf("Bad function: exited with code %d\n", code);
-		return 0;
+		return (0);
 	}
-	if (WIFSIGNALES(status))
+	if (WIFSIGNALED(status))
 	{
 		int sig = WTERMSIG(status);
 		if (sig == (SIGALRM))
 		{
 			if (verbose)
 				printf("Bad function: timed out after %u seconds\n", timeout);
+			return (0);
 		}
-		else
-		{
-			if (verbose)
-				printf("Bad function: %s\n", strsignal(sig));
-		}
-		return 0;
+		if (verbose)
+			printf("Bad function: %s\n", strsignal(sig));
+		return (0);
 	}
-	returned -1;
+	return (-1);
 }
+
