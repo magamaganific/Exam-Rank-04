18ed839955ea2725475d1868d2c5985bb5374415
Maria Fornovi Gomez <mfornovi@c2r11s4.42madrid.com>
2026-03-19 11:54:43 +0100

    studying, studying, almost had the sandbox!!

--- sandbox.c/mybox.c
+++ sandbox.c/mybox.c
@@ -1,25 +1,26 @@
 #include <unistd.h>
-#include <stdib.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
+#include <signal.h>
+#include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include <signal.h>
 #include <stdbool.h>
 
 static void do_nothing(int sig)
 {
 	(void)sig;
 }
-
 int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
 	if (!f)
-		return (-1);
+		return(-1);
+
 	int status;
 	pid_t pid = fork();
+
 	if (pid < 0)
-		return (-1);
+		return(-1);
 	if (pid == 0)
 	{
 		alarm(timeout);
@@ -31,36 +32,30 @@ int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 	sa.sa_handler = do_nothing;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-	if (sigaction(SIGALRM, &sa, NULL) < 0)
+	if(sigaction(SIGALRM, &sa, NULL) < 0)
 		return(-1);
-	pid_t r = waitpid(pid, status, 0);
-	if(r < 0)
+	pid_t r = waitpid(pid, &status, 0);
+	if (r < 0)
 	{
-		if (errno == EINTR)
-		{
-			kill(pid, SIGKILL);
-			wait(pid, NULL, 0);
-			if (verbose)
-				printf("Bad funciton: timed out after %u seconds\n", timeout);
-			return (0);
-		}
-		return (-1);
+		if (verbose)
+			printf("Bad function: timed out after %u seconds\n", timeout);
+		return(0);
 	}
 	alarm(0);
 	if(WIFEXITED(status))
 	{
 		int code = WEXITSTATUS(status);
-		if (code != 0)
+		if(code == 0)
 		{
 			if (verbose)
-				printf("Bad function: exited with code %d\n", code);
-			return (0);
+				printf("Nice function!\n");
+			return(1);
 		}
 		if (verbose)
-			printf("Nice function!\n");
-		return (1);
+			printf("Bad function: exited with code %d\n", code);
+		return(0);
 	}
-	if (WIFSIGNALED(status))
+	if(WIFSIGNALED(status))
 	{
 		int sig = WTERMSIG(status);
 		if (sig == (SIGALRM))
@@ -70,8 +65,9 @@ int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 			return(0);
 		}
 		if (verbose)
-			printf ("Bad function: %s\n", strsignal(sig));
+			printf("Bad function: %s\n", strsignal(sig));
 		return (0);
 	}
 	return(-1);
 }
+
