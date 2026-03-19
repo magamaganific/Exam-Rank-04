0b0225a4b6e7cf0dcf2158de0c2adffaa858f50a
Maria Fornovi Gomez <mfornovi@c2r11s4.42madrid.com>
2026-03-19 10:43:08 +0100

    studying, studying, almost had the popen!!

--- ft_popen.c/my_popen.c
+++ ft_popen.c/my_popen.c
@@ -4,25 +4,25 @@
 
 int ft_popen(const char *file, char *const argv[], char type)
 {
-	if (!file || ! argv || (type != 'r' && type != 'w'))
+	if (!file || !argv || (type != 'r' && type != 'w'))
 		return(-1);
 	int fd[2];
 	pid_t pid;
 
-	if(pipe(fd) < 0)
-		return (-1);
+	if (pipe(fd) < 0)
+		return(-1);
 	pid = fork();
-	if(pid < 0)
+	if (pid < 0)
 	{
-		free(fd[1]);
-		free(fd[0]);
+		close(fd[1]);
+		close(fd[0]);
 		return (-1);
 	}
 	if (pid == 0)
 	{
 		if (type == 'r')
 		{
-			if (dup2(fd[1], STDOUT_FILENO) < 0)
+			if(dup2(fd[1], STDOUT_FILENO) < 0)
 			{
 				close(fd[0]);
 				close(fd[1]);
@@ -35,7 +35,7 @@ int ft_popen(const char *file, char *const argv[], char type)
 			{
 				close(fd[0]);
 				close(fd[1]);
-				exit(-1)
+				exit(-1);
 			}
 		}
 		close(fd[0]);
@@ -43,11 +43,11 @@ int ft_popen(const char *file, char *const argv[], char type)
 		execvp(file, argv);
 		exit(-1);
 	}
-	if (type == 'r')
-	{
-		close(fd[1]);
-		return(fd[0]);
-	}
-	close(fd[0]);
-	return(fd[1]);
+		if (type == 'r')
+		{
+			close(fd[1]);
+			return(fd[0]);
+		}
+		close(fd[0]);
+		return(fd[1]);
 }
