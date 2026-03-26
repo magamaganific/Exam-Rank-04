9c26432fe0b746af92ecfc6ec8053f289499a020
Maria Fornovi Gomez <mfornovi@c2r11s6.42madrid.com>
2026-03-26 15:34:20 +0100

    studying, studying, perfect ft_open

--- ft_popen.c/my_popen.c
+++ ft_popen.c/my_popen.c
@@ -5,24 +5,25 @@
 int ft_popen(const char *file, char *const argv[], char type)
 {
 	if (!file || !argv || (type != 'r' && type != 'w'))
-		return(-1);
+		return (-1);
 	int fd[2];
+	int status;
 	pid_t pid;
 
 	if (pipe(fd) < 0)
-		return(-1);
+		return (-1);
 	pid = fork();
 	if (pid < 0)
 	{
 		close(fd[1]);
 		close(fd[0]);
-		return (-1);
+		exit(-1);
 	}
 	if (pid == 0)
 	{
 		if (type == 'r')
 		{
-			if(dup2(fd[1], STDOUT_FILENO) < 0)
+			if (dup2(fd[1], STDOUT_FILENO) < 0)
 			{
 				close(fd[0]);
 				close(fd[1]);
@@ -43,11 +44,15 @@ int ft_popen(const char *file, char *const argv[], char type)
 		execvp(file, argv);
 		exit(-1);
 	}
-	if (type == 'r')
+	else
 	{
-		close(fd[1]);
-		return(fd[0]);
+		if(type == 'r')
+		{
+			close(fd[1]);
+			return(fd[0]);
+		}
+		close(fd[0]);
+		return(fd[1]);
 	}
-	close(fd[0]);
-	return(fd[1]);
+	return(-1);
 }
